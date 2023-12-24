f334337b65302ac2a4d84b8fab66d9ae6f9f31ae
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2023-12-24 18:57:45 +0200

    structure

---
+++ raffle-client/src/App.tsx
@@ -0,0 +1,14 @@
+import {Header} from "./header";
+import "./index.css";
+import {Raffle} from "./raffle";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Raffle />
+    </>
+  );
+}
+
+export default App;
